d9607c6befdfd0ddb5b89cab00aa97679b2c730b
Vivian Hayes <vivian.xw7@gmail.com>
2024-01-07 20:39:14 -0600

    update views

---
+++ src/views/ContactView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Contact Me</h1>
+    <p>Feel free to reach out to me through the following channels:</p>
+    <ul>
+      <li>Email: vivian.xw7@gmail.com</li>
+      <li>
+        <a href="https://github.com/vivian-xw7" class="">Github</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/vivian-hayes-4629911a2/" class=""
+          >LinkedIn</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactView",
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <h1 class="hdg-1">Vivian Hayes</h1>
+
+      <p class="hdg-1">heading 1</p>
+      <p class="hdg-2">heading 2</p>
+      <p class="hdg-3">heading 3</p>
+      <p class="hdg-4">heading 4</p>
+      <p class="hdg-5">heading 5</p>
+      <p class="hdg-6">heading 6</p>
+
+      <p class="type-large">large</p>
+      <p class="type-base">base</p>
+      <p class="type-small">small</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
+// export default {
+//   name: "HomeView",
+//   components: {
+//     HelloWorld,
+//   },
+// };
 </script>
+
+<style scoped>
+@import "../styles/typography.css";
+</style>
