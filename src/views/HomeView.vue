2aedfdf9a78c213b5abf8382d45caab786486932
Vivian Hayes <vivian.xw7@gmail.com>
2024-01-15 21:11:41 -0600

    fetch API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,40 +2,42 @@
   <div class="home">
     <div class="container p-20">
       <div class="text-wrapper">
-        <!-- <h1 class="hdg-1 text-witch-night">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-witch-dusk">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-witch-dawn">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-black">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-witch-green">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-witch-purple">Vivian Hayes</h1>
-        <h1 class="hdg-1 text-witch-blue">Vivian Hayes</h1> -->
-        <h1 class="hdg-1 text-witch-space">Vivian Hayes</h1>
+        <h1
+          v-if="
+            pageContent.acf &&
+            pageContent.acf.hero_banner_heading &&
+            pageContent.acf.hero_banner_heading.text
+          "
+          class="hdg-1 text-witch-space"
+        >
+          {{ pageContent.acf.hero_banner_heading.text }}
+        </h1>
       </div>
-
-      <p class="hdg-1 text-witch-green">heading 1</p>
-      <p class="hdg-2 text-witch-green">heading 2</p>
-      <p class="hdg-3 text-witch-green">heading 3</p>
-      <p class="hdg-4 text-witch-green">heading 4</p>
-      <p class="hdg-5 text-witch-green">heading 5</p>
-      <p class="hdg-6 text-witch-green">heading 6</p>
-
-      <p class="type-large text-witch-green">large</p>
-      <p class="type-base text-witch-green">base</p>
-      <p class="type-small text-witch-green">small</p>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
-// export default {
-//   name: "HomeView",
-//   components: {
-//     HelloWorld,
-//   },
-// };
+export default {
+  data() {
+    return {
+      pageContent: {},
+    };
+  },
+  mounted() {
+    // Replace '11' with the ID of your home page or adjust as needed
+    axios
+      .get("http://code-witch.local/wp-json/wp/v2/pages/11")
+      .then((response) => {
+        this.pageContent = response.data;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  },
+};
 </script>
 
 <style scoped></style>
