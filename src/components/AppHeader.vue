a38df89e3511f1f2f52baeeddf39714811de3789
Vivian Hayes <vivian.xw7@gmail.com>
2024-01-21 19:20:22 -0600

    remove axios from header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,59 +5,35 @@
         class="relative z-10 flex justify-center items-center h-full gap-x-12"
       >
         <router-link
-          v-for="(label, key) in topNavContent"
-          :key="key"
-          to="/"
-          class="hdg-6 router-link"
+          v-for="route in routes"
+          :key="route.name"
+          :to="route.path"
+          class="hdg-6 router-link lowercase"
         >
           <span class="comment-span">// </span>
-          <span class="text-span">{{ label }}</span>
+          <span class="text-span">{{ route.name }}</span>
         </router-link>
-
-        <!-- <router-link to="/" class="hdg-6 router-link">
-          <span class="comment-span">// </span>
-          <span class="text-span">home</span>
-        </router-link>
-        <router-link to="/projects" class="hdg-6 router-link">
-          <span class="comment-span">// </span>
-          <span class="text-span">projects</span>
-        </router-link>
-        <router-link to="/contact" class="hdg-6 router-link">
-          <span class="comment-span">// </span>
-          <span class="text-span">contact</span>
-        </router-link> -->
       </div>
     </div>
   </nav>
 </template>
 
 <script>
+import router from "../router/index";
 import { RouterLink } from "vue-router";
-import axios from "axios";
 
 export default {
   data() {
     return {
       pageContent: {},
+      routes: [],
     };
   },
   components: {
     RouterLink,
   },
-  computed: {
-    topNavContent() {
-      return this.pageContent.acf?.header_top_nav_content || {};
-    },
-  },
   mounted() {
-    axios
-      .get("http://code-witch.local/wp-json/wp/v2/pages/11")
-      .then((response) => {
-        this.pageContent = response.data;
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    this.routes = router.getRoutes().filter((route) => route.name);
   },
 };
 </script>
